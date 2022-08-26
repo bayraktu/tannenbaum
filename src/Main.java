cceb08c2ffdae7670e3b6529ca3f5fbe48747c7e
ugur <bayraktu@live.de>
2022-08-26 18:18:18 +0200

    Ugur grüßt

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("wir starten");
+
+        Ugur ugur = new Ugur();
+
+        System.out.println(ugur.greetings());
     }
 }
--- src/Ugur.java
+++ src/Ugur.java
@@ -1,2 +1,6 @@
 public class Ugur {
+
+    public String greetings(){
+        return "Hello Peaople";
+    }
 }
