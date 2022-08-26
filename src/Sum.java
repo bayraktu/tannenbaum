0ba56bf15da9ce70aaa51bf6bc4f9a3c4ed1e746
Sum2411 <sahinturk34@hotmail.com>
2022-08-26 19:02:35 +0200

    whatsthis

--- src/Sum.java
+++ src/Sum.java
@@ -1,7 +1,18 @@
 public class Sum {
 
+    private String dieBesteSache;
+
+
+
+    public String getDieBesteSache() {
+        return dieBesteSache="schwimmen";
+    }
+
+
     public String greetings(){
         return "Hello Vienna,was machen sachen?";
     }
+
+
 
 }
