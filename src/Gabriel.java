bb953ec73c2d21092ae12a9d0dc92ecfe8bde8f2
Gabriel <loltroll.cool@gmail.com>
2022-08-26 18:57:16 +0200

    getUnserName erstellt

--- src/Gabriel.java
+++ src/Gabriel.java
@@ -3,4 +3,8 @@ public class Gabriel {
     public String greetsGabriel(){
         return "Das ist die Klasse von Gabriel, hallo!";
     }
+
+    public String getUnserName(){
+        return "Sum, Mueyessernur, Yasin, Ugur und Gabriel!";
+    }
 }
